aab2d2c2d96fae2da679f86a557dfb746adb8534
Andrew Swartzlander <aswartzlander2@gmail.com>
2026-08-23 20:47:35 -0400

    Update github-pages-config.js

--- ipdata/github-pages-config.js
+++ ipdata/github-pages-config.js
@@ -1,7 +1,7 @@
 // This file is copied to the GitHub Pages artifact as runtime-config.js.
-// It points the static site to the separately hosted Robo IP Data API.
+// /ipdata (no trailing slash) is the separately hosted Robo IP Data API.
 window.ROBO_NETWORK_CONFIG = Object.freeze({
   mode: 'static',
   apiBase: '',
-  publicIpEndpoint: 'https://api.robo-universe.com/ipdata'
+  publicIpEndpoint: 'https://apis.robo-universe.com/ipdata'
 });
